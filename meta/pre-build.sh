7526ff15ebaf08f8f0bd6446584d33ebeb91c2d3
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-09-11 00:09:59 -0400

    fix: quote the pre-build script to make it run
    Signed-off-by: Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>

--- meta/pre-build.sh
+++ meta/pre-build.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+set -eu
+
+set_output() {
+  echo "::set-output name=$1::$2"
+}
+info() {
+  echo "::info::$*"
+}
+warn() {
+  echo "::warning file=entrypoint.sh::$*"
+}
+error() {
+  echo "::error file=entrypoint.sh::$*"
+}
+
 info "Running pre-build script"
 info "This script modifies the json file at GITHUB_EVENT_PATH"
 
@@ -7,4 +22,4 @@ info "This script modifies the json file at GITHUB_EVENT_PATH"
 pip install --upgrade pip
 pip install PyGithub
 
-python3 meta/update_json.py
+python3 update_json.py
